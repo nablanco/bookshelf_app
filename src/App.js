5237d9a1d39dc32b5bd574cf9af2ccab48b79718
Nick Blanco <nablanco92@gmail.com>
2024-08-10 17:31:28 +0200

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
 
+import Navigation from "./components/navigation/navigation";
+import Home from "./components/main/home/home";
+import Library from "./components/main/library/library";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Home />
+      <Library />
+      <Footer />
     </div>
   );
 }
