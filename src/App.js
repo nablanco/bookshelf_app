03c8564c9b878c79a3bebd7194dc41224363534f
Nick Blanco <nablanco92@gmail.com>
2024-08-11 13:50:28 +0200

    Add react-route. Add home and library routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 
 import Navigation from "./components/navigation/navigation";
 import Home from "./components/main/home/home";
 import Library from "./components/main/library/library";
 import Footer from "./components/footer/footer";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <div className="App">
+    <Box className="App">
       <Navigation />
-      <Home />
-      <Library />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
       <Footer />
-    </div>
+    </Box>
   );
 }
 
