e46085c5158da695ba1a016445972d5ff08f20af
Nicholas Blanco <nablanco92@gmail.com>
2024-08-16 12:24:57 +0200

    BLA-13 Add empty library state

--- src/components/main/library/library.js
+++ src/components/main/library/library.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, Text, Heading } from "@chakra-ui/react";
 
 const Library = () => {
-  return <div>library</div>;
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  const HandleEmptyStatus = () => {
+    setIsEmpty(!isEmpty);
+  };
+
+  if (isEmpty) {
+    return (
+      <Box>
+        <Heading>Library</Heading>
+        <Text>You haven&apos;t added any books! Click below to add a book</Text>
+        <Button onClick={HandleEmptyStatus}>Add a book!</Button>
+      </Box>
+    );
+  }
+  return (
+    <Box>
+      Library
+      {console.log(isEmpty)}
+      <Button onClick={HandleEmptyStatus}>Remove a book!</Button>
+    </Box>
+  );
 };
 
 export default Library;
