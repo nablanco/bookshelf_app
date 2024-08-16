6fd8ec9a6fc02c144c2ba7ff7e73059703b2bbab
Nicholas Blanco <nablanco92@gmail.com>
2024-08-16 17:14:33 +0200

    BLA-14 Add basic chakra table w/ dummy book data

--- src/components/main/library/library.js
+++ src/components/main/library/library.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 import { Box, Button, Text, Heading } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 const Library = () => {
   const [isEmpty, setIsEmpty] = useState(true);
@@ -22,6 +32,36 @@ const Library = () => {
       Library
       {console.log(isEmpty)}
       <Button onClick={HandleEmptyStatus}>Remove a book!</Button>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>Your main library</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Author</Th>
+              <Th>Title</Th>
+              <Th>Form</Th>
+              <Th>Topics</Th>
+              <Th>Historical Era</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Author 1</Td>
+              <Td>Book 1</Td>
+              <Td>Fiction</Td>
+              <Td>Politics</Td>
+              <Td>Ancient Greece</Td>
+            </Tr>
+            <Tr>
+              <Td>Author 2</Td>
+              <Td>Book 2</Td>
+              <Td>Non-fiction</Td>
+              <Td>Art</Td>
+              <Td>South America</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
