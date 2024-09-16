02e3364bfdf31412b11d34142f1c51c554f9a089
Nicholas Blanco <nablanco92@gmail.com>
2024-09-16 10:58:15 +0200

    Add Chakra input and update stlying

--- src/components/main/home/home.js
+++ src/components/main/home/home.js
@@ -1,11 +1,28 @@
 import React from "react";
+import {
+  Flex,
+  Stack,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+
 import Library from "../library/library";
+import { SearchIcon } from "@chakra-ui/icons";
 
 const Home = () => {
   return (
-    <div>
-      <Library />
-    </div>
+    <Flex direction="column" align="center" mt="24px">
+      <Stack spacing={10} width="90%">
+        <InputGroup maxW="600px" alignSelf="center">
+          <Input placeholder="Search Google Books" />
+          <InputRightElement>
+            <SearchIcon />
+          </InputRightElement>
+        </InputGroup>
+        <Library />
+      </Stack>
+    </Flex>
   );
 };
 
